1c457a980efc12503d9794e6d0ab0320124b740d
Ola Ouso <ouso.ola@gmail.com>
2020-10-30 21:21:59 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -11,7 +11,7 @@ class Pet extends React.Component {
             {this.props.pet.name} {this.props.pet.gender === "male" ? '♂':'♀'}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
             <p>Age: PET AGE</p>
